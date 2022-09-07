78235d2fbe2c87b2bfdfc2194249f8ae13c4fdcc
Mohsen Hariri <mhariri68@gmail.com>
2022-09-07 13:40:33 -0400

    promise was added

--- ray_core.py
+++ ray_core.py
@@ -1,28 +1,21 @@
 import glob
+
 import numpy as np
 import openslide
-import matplotlib.pyplot as plt
-from skimage import io, color, img_as_ubyte
-import logging
-import os
-import numpy as np
-from skimage import io, color, img_as_ubyte
-from distutils.util import strtobool
-from skimage.filters import threshold_otsu, rank
+import ray
+from skimage import color, img_as_ubyte, io
+from skimage.filters import rank
 from skimage.morphology import disk
-from sklearn.cluster import KMeans
-from skimage import exposure
-
 
 @ray.remote
 def load_and_run_minimumPixelIntensityNeighborhoodFiltering(fname):
     try:
-        osh=openslide.OpenSlide(fname)
-        dim=2
-        img=osh.read_region((0,0),dim,osh.level_dimensions[dim])
-        img= np.asarray(img)[:, :, 0:3]
+        osh = openslide.OpenSlide(fname)
+        dim = 2
+        img = osh.read_region((0, 0), dim, osh.level_dimensions[dim])
+        img = np.asarray(img)[:, :, 0:3]
 
-        disk_size =  5
+        disk_size = 5
         threshold = 210
 
         img = color.rgb2gray(img)
@@ -30,8 +23,19 @@ def load_and_run_minimumPixelIntensityNeighborhoodFiltering(fname):
         selem = disk(disk_size)
 
         imgfilt = rank.minimum(img, selem)
-        imgout =  imgfilt > threshold
-        io.imsave(f"/opt/{fname.replace('/data/','').replace('.svs','_mask.png')}",img_as_ubyte(imgout))
+        imgout = imgfilt > threshold
+        io.imsave(
+            f"/opt/{fname.replace('/data/','').replace('.svs','_mask.png')}",
+            img_as_ubyte(imgout),
+        )
         return imgout.mean()
     except:
         return -1
+
+files = glob.glob("/data/*.svs")
+
+futures = [
+    load_and_run_minimumPixelIntensityNeighborhoodFiltering.remote(f) for f in files
+]
+
+ray.get(futures)
